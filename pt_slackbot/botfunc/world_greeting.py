9a92e34c66d75537fc9b762ca647824f803d26e1
hirohis sano <hrs.sano645@gmail.com>
2020-08-08 13:43:37 +0900

    簡単な内容な返答結果を用意

--- pt_slackbot/botfunc/world_greeting.py
+++ pt_slackbot/botfunc/world_greeting.py
@@ -1,1 +1,14 @@
+# coding:utf-8
 # 世界の挨拶を返すbot
+
+from typing import Union
+
+
+def call_function(match_group: str) -> Union[str, None]:
+    """
+    botの結果を返すfunction
+    """
+
+    result = "hello!!"
+
+    return result
