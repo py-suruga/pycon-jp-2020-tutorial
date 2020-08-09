d5c2fc2782dd035b72cfe77db1752e4373d83d02
hirohis sano <hrs.sano645@gmail.com>
2020-08-09 11:06:46 +0900

    fix cooment

--- pt_slackbot/tests/test_world_greeting.py
+++ pt_slackbot/tests/test_world_greeting.py
@@ -5,11 +5,10 @@ import pytest
 @pytest.fixture()
 def setup_non_random(monkeypatch):
 
-    # ランダムで返す結果を固定にする
+    # ランダムで返す結果を固定にする:アイサツリストを1行のみにして、結果的に1つの結果のみを返すようにする
     monkeypatch.setattr(world_greeting, "GREETING_LIST", [("こんにちは！", ":jp:")])
 
 
-# @pytest.mark.parametrize()
 def test_greeting_result(setup_non_random):
     """
     アイサツの文章が決めたとおりに出力されるかを確認
