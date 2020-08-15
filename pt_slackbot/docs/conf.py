d6bdd1c180ef248a81ba665bb49f241a19a8ae62
hirohis sano <hrs.sano645@gmail.com>
2020-08-15 10:16:45 +0900

    change theme: using sphinx_rtd_theme

--- pt_slackbot/docs/conf.py
+++ pt_slackbot/docs/conf.py
@@ -4,16 +4,26 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sphinx_rtd_theme
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
+
+# import os
+# import sys
+
+# sys.path.insert(0, os.path.abspath('.'))
+
+# TODO:2020-08-15 この部分はsphinx-quickstartで生成されたコードを変えています。
+# チュートリアル全体でpathlibを扱っているのでpathlibでパスを生成しています。
+from pathlib import Path
 import sys
 
-sys.path.insert(0, os.path.abspath("..\\"))
+sys.path.insert(0, str(Path("../")))
 
 
 # -- Project information -----------------------------------------------------
@@ -31,7 +41,7 @@ release = "2020.07.24"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -54,7 +64,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
